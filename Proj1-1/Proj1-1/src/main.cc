35b583d33e10d53463625211369a66491e0b26f0
LiuHX01 <2539807128@qq.com>
2022-10-15 22:52:51 +0800

    run2 pass

--- Proj1-1/Proj1-1/src/main.cc
+++ Proj1-1/Proj1-1/src/main.cc
@@ -1,49 +1,51 @@
 #include "world.h"
 #include <cstdio>
-#include <fstream>
-#include <cstring>
 #include <cstdlib>
-#include <string>
+#include <cstring>
+#include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
 char trace_file[15];
 char path[30] = "../traces/";
 
-int main(int argc, char *argv[]) {
-	Cache MyCache(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
-	strcpy(trace_file, argv[6]);
+int main(int argc, char* argv[]) {
+    Cache MyCache(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+    strcpy(trace_file, argv[6]);
 
-	strcat(path, trace_file);
-	strcat(path, ".txt");
+    strcat(path, trace_file);
+    strcat(path, ".txt");
 
-	int a = 1;
+    int a = 1;
 
-	ifstream in(path);
-	string s;
-	while(getline(in, s)) {
-		long int addr;
-		string addrs = s.substr(2, 8);
-		addr = strtol(addrs.c_str(), NULL, 16);
+    ifstream in(path);
+    string s;
+    while (getline(in, s)) {
+        unsigned int addr;
+        string addrs = s.substr(2, 8);
+        addr = strtol(addrs.c_str(), NULL, 16);
 
-		cout << "# " << a << " : ";
+        // cout << "# " << a << " : ";
 
-		if (s[0] == 'w' || s[0] == 'W') {
-			cout << "Write " << MyCache.dec2Hex(addr) << endl;
-			cout << "L1 Write: " << MyCache.dec2Hex(addr);
-			MyCache.write(addr);
-		} else if (s[0] == 'r' || s[0] == 'R') {
-			cout << "Read " << MyCache.dec2Hex(addr) << endl;
-			cout << "L1 Read: " << MyCache.dec2Hex(addr);
-			MyCache.read(addr);
-		} else {
-			cout << "Operate ERROR!" << endl;
-			break;
-		}
-		a++;
-		// cout << a << endl;
-		// if (a == 20) break;
-	}
+        if (s[0] == 'w' || s[0] == 'W') {
+            // cout << "Write " << MyCache.dec2Hex(addr) << endl;
+            // cout << "L1 Write: " << MyCache.dec2Hex(addr);
+            MyCache.write(addr);
+        }
+        else if (s[0] == 'r' || s[0] == 'R') {
+            // cout << "Read " << MyCache.dec2Hex(addr) << endl;
+            // cout << "L1 Read: " << MyCache.dec2Hex(addr);
+            MyCache.read(addr);
+        }
+        else {
+            cout << "Operate ERROR!" << endl;
+            break;
+        }
+        a++;
+        // cout << a << endl;
+        // if (a == 20) break;
+    }
 
-	// MyCache.printCache();
+    MyCache.printCache();
 }
--- Proj1-1/Proj1-1/src/world.h
+++ Proj1-1/Proj1-1/src/world.h
@@ -1,54 +1,51 @@
-#include <utility>
 #include <string>
+#include <utility>
 
 using namespace std;
 
 class Cache {
-private:
+  private:
     int L1_BLOCKSIZE;
     int L1_SIZE;
     int L1_ASSOC;
     int L1_REPLACEMENT_POLICY;
     int L1_WRITE_POLICY;
-    
+
     int L1_BLOCK_COUNT;
     int L1_GROUP_COUNT;
-    
+
     int L1_TAG_BITCOUNT;        // 高
     int L1_GROUP_BITCOUNT;      // 中
     int L1_BLOCKSIZE_BITCOUNT;  // 低
 
-    // unsigned int **L1_set;
     unsigned int L1_set[1024][8];
-    // unsigned short ***L1_state;
-    unsigned short L1_state[1024][8][4];
+    unsigned int L1_state[1024][8][4];
 
-    int VALID;
-    int DIRTY;
+    int VALID;  // 0可用 1占用
+    int DIRTY;  // 0不脏 1脏
     int COUNT_BLOCK;
     int COUNT_SET;
-    // 0: valid, 1: dirty, 2: count_block, 3: count_set
-    // 0可用       0不脏
 
     int LRU;
     int LFU;
     int WBWA;
     int WTNA;
 
-public:
-    int r;
-    int r_miss;
-    int w;
-    int w_miss;
+    int r_count;
+    int r_miss_count;
+    int w_count;
+    int w_miss_count;
 
-    Cache(int bsize, int size, int assoc, int repolicy, int wpolicy); // 构造函数
+    bool DEBUG;
 
-    void read(long int addr);
-    void write(long int addr);
-    unsigned int getIndex(long int addr);
-    unsigned int getTag(long int addr);
+  public:
+    Cache(int bsize, int size, int assoc, int repolicy, int wpolicy);  // 构造函数
+    void read(unsigned int addr);
+    void write(unsigned int addr);
+    unsigned int getIndex(unsigned int addr);
+    unsigned int getTag(unsigned int addr);
     pair<bool, unsigned int> missOrHit(unsigned int index, unsigned int tag);
-    unsigned int findMaxCount(unsigned int index);
+    unsigned int selectReplaced(unsigned int index);
     string dec2Hex(unsigned int x);
     void printCache();
     void printSingleSet(unsigned int index);
